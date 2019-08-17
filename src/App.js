d9e85edaf22c4dbfa1f8c4529d015bc8f21559e2
xiaoweixu0530 <Your new email>
2019-08-17 23:06:47 +0200

    add env check

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const env = process.NODE_ENV;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const checkEnv = env => {
+   if (env==='development') {
+       return 'DEV'
+   } else {
+       return 'NON-DEV'
+   }
+  };
+
+  return <React.Fragment> {checkEnv(env)} </React.Fragment>;
 }
 
 export default App;
