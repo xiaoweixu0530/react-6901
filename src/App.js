3ef7c0e7f932111e9d08ec12e30c730711ef7b5a
xiaoweixu0530 <Your new email>
2019-08-19 22:13:03 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React from "react";
+import { Child } from "./components/child";
 import "./App.css";
 
 const env = process.env.NODE_ENV;
 
-function App() {
-  const checkEnv = env => {
-   if (env==='development') {
-       return 'DEV'
-   } else {
-       return 'NON-DEV'
-   }
+class App extends React.Component {
+  state = {
+    test: "123"
+  };
+  checkEnv = env => {
+    if (env === "development") {
+      return "DEV";
+    } else {
+      return "NON-DEV ";
+    }
   };
 
-  return <React.Fragment> {checkEnv(env)} </React.Fragment>;
+  onchange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { test } = this.state;
+    return (
+      <React.Fragment>
+        <div>
+          {this.checkEnv(env)} but still nothing is really happening though you
+          could change text
+        </div>
+        <Child test={test} onChange={this.onchange} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/child.js
+++ src/components/child.js
@@ -1,3 +1,23 @@
-export const child = () => {
+import React from "react";
+import { Input, Tooltip, Icon } from "antd";
 
-}
+export const Child = props => {
+  const { test, onChange } = props;
+
+  return (
+    <React.Fragment>
+      <Input
+        name="test"
+        value={test}
+        onChange={onChange}
+        placeholder="Enter your username"
+        prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+        suffix={
+          <Tooltip title="Extra information">
+            <Icon type="info-circle" style={{ color: "rgba(0,0,0,.45)" }} />
+          </Tooltip>
+        }
+      />
+    </React.Fragment>
+  );
+};
